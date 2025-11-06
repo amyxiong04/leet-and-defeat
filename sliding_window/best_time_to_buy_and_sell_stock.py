da4b022be3b0ec62ed3ae281d89b8c9a246b8208
Amy Xiong <amyxiongg@gmail.com>
2025-11-05 21:58:48 -0800

    2025-11-05

--- sliding_window/best_time_to_buy_and_sell_stock.py
+++ sliding_window/best_time_to_buy_and_sell_stock.py
@@ -8,7 +8,8 @@ class Solution:
             if prices[left] < prices[right]:
                 profit = prices[right] - prices[left]
                 maxProfit = max(maxProfit, profit)
-            else:
+            else: # if any cheaper buy price, immediately go to that price. even if it doesnt end being max profit, the max so far is saved
+                # but if max is later, than the profit will be more since this number is smaller (consider [3, 9, 2, 4, 7])
                 left = right
             right += 1
 
