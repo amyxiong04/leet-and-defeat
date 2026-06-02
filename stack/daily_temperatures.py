fac3cd720cc361f5bac309192e31ea2946506a14
amyxiong04 <amyxiongg@gmail.com>
2026-06-02 13:37:36 -0700

    daily temperatures

--- stack/daily_temperatures.py
+++ stack/daily_temperatures.py
@@ -1,11 +1,25 @@
+# class Solution:
+#     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+#         stack = []  # keeps track of the indices of days where we hvent found warmer temp
+#         res = [0] * len(temperatures)  # stores number of days until next warmer temp for each day
+#         for i, temp in enumerate(temperatures):
+#             while stack and temperatures[i] > temperatures[stack[-1]]:
+#                 index = stack.pop()
+#                 res[index] = i - index
+#             stack.append(i)
+
+#         return res
+
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack = []  # keeps track of the indices of days where we hvent found warmer temp
-        res = [0] * len(temperatures)  # stores number of days until next warmer temp for each day
-        for i, temp in enumerate(temperatures):
+        stack = [] # keep track of earlier days' indexes w unknown answer
+        ans = [0] * len(temperatures)
+
+        for i in range(0, len(temperatures)):
             while stack and temperatures[i] > temperatures[stack[-1]]:
-                index = stack.pop()
-                res[index] = i - index
+                ans[stack[-1]] = i - stack[-1]
+                stack.pop() # remove that day now, since its answer is known
             stack.append(i)
 
-        return res
+        return ans
+        
