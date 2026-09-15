7771b9f979822108adcb8f1c78d23b5edfef149f
Amy Xiong <amyxiongg@gmail.com>
2026-09-15 00:19:00 -0400

    9-15

--- dynamic programming/decode_ways.py
+++ dynamic programming/decode_ways.py
@@ -1,25 +1,15 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
-        dp = [0] * (n + 1)
 
-        # Empty string has 1 valid decoding
-        dp[n] = 1
+        dp = [0] * (n + 1) # dp[i] = number of ways to decode the first i chars
 
-        # Process from the end of the string back to the front
-        for i in range(n - 1, -1, -1):
-            # '0' cannot be decoded alone
-            if s[i] == '0':
-                dp[i] = 0
-            else:
-                # Use one digit
-                dp[i] = dp[i + 1]
+        dp[0] = 1 # empty prefix
+        dp[1] = 0 if s[0] == "0" else 1
 
-                # Use two digits if they form a number from 10 to 26
-                if i + 1 < n and (s[i] == '1' or (s[i] == '2' and s[i + 1] <= '6')):
-                    dp[i] += dp[i + 2]
-
-        return dp[0]
+        for i in range(2, len(s) + 1):
+            one_digit = int(s[i - 1])
+            two_digits = int(s[i - 2:i])
     
 
     
