06413f9abe5cc33a5294fbbf86a5b6f548c0f146
Amy Xiong <amyxiongg@gmail.com>
2026-09-18 00:15:53 -0400

    9-18

--- dynamic programming/longest_palindromic_substring.py
+++ dynamic programming/longest_palindromic_substring.py
@@ -1,54 +1,31 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
+        dp = [[False] * n for _ in range(n)] # dp[i][j] = whether s[i:j+1] is a palindrome
 
-        if n == 0:
-            return ""
-
-        dp = [[False] * n for _ in range(n)]
-        start = 0
+        max_start = 0
         max_len = 1
-
-        # every single character is a palindrome
+        
+        # length 1: all chars by themselves are palindromes
         for i in range(n):
             dp[i][i] = True
 
-        # check all substring lengths from 2 up to n
-        for length in range(2, n + 1):
-            for i in range(n - length + 1):
-                j = i + length - 1
+        # length 2
+        for i in range(n - 1):
+            if s[i + 1] == s[i]:
+                dp[i][i + 1] = True
 
-                if s[i] == s[j]:
-                    if length <= 3 or dp[i + 1][j - 1]:
+                max_len = 2
+                max_start = i
+
+        # length 3 and above
+        for i in range(n - 1, -1, -1):
+            for j in range(i + 2, n):
+                if s[i] == s[j] and dp[i + 1][j - 1]:
                         dp[i][j] = True
 
-                        if length > max_len:
-                            start = i
-                            max_len = length
+                        if (j - i + 1) > max_len:
+                            max_len = j - i + 1
+                            max_start = i
 
-        return s[start:start + max_len]
-    
-
-    # but usually this approach is more intuitive (expand around center):
-    def longestPalindrome(self, s: str) -> str:
-        if not s:
-            return ""
-
-        res = ""
-
-        def expand(left, right):
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1
-                right += 1
-            return s[left + 1:right]
-
-        for i in range(len(s)):
-            odd = expand(i, i)
-            if len(odd) > len(res):
-                res = odd
-
-            even = expand(i, i + 1)
-            if len(even) > len(res):
-                res = even
-
-        return res
+        return s[max_start: max_start + max_len]
